b1b456547828fc3c8a37a1402ba534300354ec6b
hung20012004 <hamanhhung20012004@gmail.com>
2024-05-23 12:03:20 +0700

    Initial commit

---
+++ database/migrations/2024_05_21_165444_create_on_sale_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('on_sale', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('laptop_id')->constrained()->cascadeOnDelete();
+            $table->double('discount');
+            $table->dateTime('time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('on_sale');
+    }
+};
