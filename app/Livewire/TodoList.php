be2d3070935c546134ab739198323a5504e78f86
hung20012004 <hamanhhung20012004@gmail.com>
2024-05-24 15:31:02 +0700

    livewire

---
+++ app/Livewire/TodoList.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class TodoList extends Component
+{
+    public $tasks = [];
+    public $newTask = '';
+
+    public function addTask()
+    {
+        if (!empty($this->newTask)) {
+            $this->tasks[] = $this->newTask;
+            $this->newTask = '';
+        }
+    }
+    public function removeTask($index)
+    {
+        unset($this->tasks[$index]);
+        $this->tasks = array_values($this->tasks);
+    }
+
+    public function render()
+    {
+        return view('livewire.todo-list');
+    }
+}
---
+++ resources/views/livewire/todo-list.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <input type="text" wire:model="newTask" placeholder="Add a new task">
+    <ul>
+        @foreach($tasks as $index=> $task)
+            <li>
+                {{ $task }}
+                <button wire:click="removeTask({{ $index }})">Remove</button>
+            </li>
+        @endforeach
+    </ul>
+    <button wire:click="addTask">Add Task</button>
+</div>
