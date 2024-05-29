a87c4d5eae9ebd22f3148f444407dd3ccf757e3c
hung20012004 <hamanhhung20012004@gmail.com>
2024-05-29 17:43:07 +0700

    crud

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use app\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -22,7 +24,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        return view('user.manager.create-user');
     }
 
     /**
@@ -30,38 +32,68 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //dd($request->input('role'));
+        // $validatedData = $request->validate([
+        //     'name' => 'required|string|max:255',
+        //     'email' => 'required|email|unique:users,email',
+        //     'password' => 'required|string|min:8', // Yêu cầu mật khẩu có ít nhất 8 ký tự
+        //     'role' => 'required|string|in:admin,user',
+        // ]);
+        $user = new User();
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = Str::password(8);
+        $user->role = $request->input('role');
+        $user->save();
+        return redirect()->route('users.index', $user)->with('success', 'User created successfully!');
     }
-
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(User $user)
     {
-        //
+        $user = User::findOrFail($user->id);
+
+        return view('user.manager.show-user', compact('user'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(User $user)
     {
-        //
+        $user = User::findOrFail($user->id);
+
+        return view('user.manager.edit-user', compact('user'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request,User $user)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email,'.$user->id,
+            // Thêm các quy tắc validation cho các trường thông tin khác nếu cần
+        ]);
+
+        // Cập nhật thông tin người dùng với dữ liệu mới
+        $user->update($validatedData);
+
+        // Chuyển hướng người dùng đến trang hiển thị thông tin chi tiết của họ
+        return redirect()->route('users.index', $user)->with('success', 'User information updated successfully!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(User $user)
     {
-        //
+         // Xóa người dùng
+         $user->delete();
+
+         // Chuyển hướng người dùng đến trang danh sách người dùng
+         return redirect()->route('users.index')->with('success', 'User deleted successfully!');
     }
 }
