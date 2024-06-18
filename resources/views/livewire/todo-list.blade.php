df1aa1b52d8c4ca48772ed711e831dfdbb68b4b0
Hoang Manh <154540545+Duy-2706@users.noreply.github.com>
2024-06-18 12:35:28 +0700

    gfđffggfgf

--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -1,10 +1,6 @@
 <div>
     <input type="text" wire:model="newTask" placeholder="Add a new task">
     <button wire:click="addTask">Add Task</button>
-    <ul>
-        @foreach($tasks as $index=> $task)
-   <input type="text" wire:model="newTask" placeholder="Add a new task">
-    <button wire:click="addTask">Add</button>
 
     <ul>
         @foreach($tasks as $index => $task)
